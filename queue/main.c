ece32d514691b25e3bacd0451dff734825ea3585
bluesky641 <3352167818@qq.com>
2020-08-23 00:08:20 +0800

    队列使用结构体表示

--- queue/main.c
+++ queue/main.c
@@ -8,26 +8,48 @@
     个队列当中，新来的人总是站在队列的最后面，来得越早的人越靠前，也就越早能买到票，就是先来的人先服务，我
     们称为“先进先出”（First In First Out，FIFO）原则。
 */
-
+/*
+    题目：加密字符串解密
+    规则：首先将第 1 个数删除，紧接着将第 2 个数放到这串数的末尾，再将第 3 个数删除并将第 4 个数放到这串数的末尾，再将
+    第 5 个数删除……直到剩下最后一个数，将最后一个数也删除。按照刚才删除的顺序，把这些删除的数连在一起就是解密字符串。给出加密
+    过的一串数是“6 3 1 7 5 8 9 2 4”。
+*/
 #include <stdio.h>
 #include <windows.h>
 
+#define N 9
+//结构体实现队列操作
+struct queue
+{
+    int data[100];
+    int head;
+    int tail;
+};
+
 int main()
 {
-    int q[102] = {6,3,1,7,5,8,9,2,4},head,tail;
+    struct queue q;
     int i;
     //初始化队列
-    head = 0; //队列中有9个元素，tail指向队尾的后一个位置
-    tail = 9;
-    while(head < tail)//当队列不为空的时候执行循环
+    q.head = 0; //队列中有9个元素，tail指向队尾的后一个位置
+    q.tail = 0;
+    for(i=0;i<N;i++)
+    {
+        printf("Please input %dth number:",i+1);
+        scanf("%d",&q.data[q.tail]);
+        q.tail++;
+    }
+    while(q.head < q.tail)//当队列不为空的时候执行循环
     {
         //打印队首并将队首出队
-        printf("%d ",q[head]);
-        head++;
+        printf("%d ",q.data[q.head]);
+        q.head++;
         //将第二个添加到队尾
-        q[tail] = q[head];
-        tail++;
-        head++;
+        q.data[q.tail] = q.data[q.head];
+        q.tail++;
+        //再将队首出列
+        q.head++;
     }
     system("pause");
+    return 0;
 }
